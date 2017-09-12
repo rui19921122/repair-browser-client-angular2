820c8ccedaa8c4077fc8c2de6f85f8dd8c2b7816
HanRui <rui19921122@qq.com>
2017-09-12 09:30:14 +0800

    go ahead

---
+++ src/app/repair-history-collect/content/repair-plan-detail-card/repair-plan-detail-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repair-plan-detail-card',
+  templateUrl: './repair-plan-detail-card.component.html',
+  styleUrls: ['./repair-plan-detail-card.component.css']
+})
+export class RepairPlanDetailCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
