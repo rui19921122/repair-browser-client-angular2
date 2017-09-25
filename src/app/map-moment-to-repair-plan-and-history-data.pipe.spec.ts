c85fb648eebc1d426df28c34bac09d83985a2340
HanRui <rui19921122@qq.com>
2017-09-25 09:22:15 +0800

    终于解决了tooltip不显示的bug了，angular里模板变量用函数容易出事啊

---
+++ src/app/map-moment-to-repair-plan-and-history-data.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { MapMomentToRepairPlanAndHistoryDataPipe } from './map-moment-to-repair-plan-and-history-data.pipe';
+
+describe('MapMomentToRepairPlanAndHistoryDataPipe', () => {
+  it('create an instance', () => {
+    const pipe = new MapMomentToRepairPlanAndHistoryDataPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
